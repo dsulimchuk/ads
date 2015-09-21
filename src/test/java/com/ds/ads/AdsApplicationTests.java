64a4195d551c234074e2053fe6b13a05d975bd50
ds <dserge.spb@gmail.com>
2015-09-21 22:48:09 +0300

    misc

--- src/test/java/com/ds/ads/AdsApplicationTests.java
+++ src/test/java/com/ds/ads/AdsApplicationTests.java
@@ -1,19 +1,40 @@
 package com.ds.ads;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.ds.ads.controllers.MainPageCtrl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AdsApplication.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
 public class AdsApplicationTests {
 
+	private MockMvc mvc;
+	
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new MainPageCtrl()).build();
+	}
+
 	@Test
-	public void contextLoads() {
-	   
+	public void test() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(content().string(equalTo("Intentionally left blank")));
 	}
 
 }
