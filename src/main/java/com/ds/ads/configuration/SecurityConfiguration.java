8c76a6c7812542a1ced58cf4f7a8ac34f4e9e01a
ds <dserge.spb@gmail.com>
2015-11-13 23:45:25 +0300

    initial add security config

---
+++ src/main/java/com/ds/ads/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.ds.ads.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by ds on 14/11/15.
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        System.out.println("!!!!!!!!!!!!");
+        builder.
+                inMemoryAuthentication().
+                withUser("user").
+                password("user1").roles("USER");
+
+    }
+
+}
